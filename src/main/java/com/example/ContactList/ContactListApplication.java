b19489b54c1c8d52fc906da68b8d13b5a39b8253
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2023-01-11 19:41:29 +0200

    debugging cors problem

--- src/main/java/com/example/ContactList/ContactListApplication.java
+++ src/main/java/com/example/ContactList/ContactListApplication.java
@@ -2,12 +2,25 @@ package com.example.ContactList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ContactListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactListApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/getContacts").allowedOrigins("http://localhost:8080","https://contact-list.herokuapp.com/api/contactses");
+			}
+		};
 	}
 
 }
--- src/main/java/com/example/ContactList/web/ContactsController.java
+++ src/main/java/com/example/ContactList/web/ContactsController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,6 +17,7 @@ public class ContactsController {
 	@Autowired
 	private ContactsRepository crepository;
 	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value= {"/getContacts"})
 	public @ResponseBody List<Contacts> getContacts() {
 		return (List<Contacts>) crepository.findAll();
